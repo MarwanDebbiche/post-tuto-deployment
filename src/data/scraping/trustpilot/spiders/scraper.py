703e242322482d4df8532c789d06876217aa5cb8
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-10-28 20:26:18 +0100

    fix error in scraping

--- src/data/scraping/trustpilot/spiders/scraper.py
+++ src/data/scraping/trustpilot/spiders/scraper.py
@@ -12,8 +12,10 @@ class Pages(scrapy.Spider):
         company_logo = response.xpath('//img[@class="business-unit-profile-summary__image"]/@src').extract_first()
         company_website = response.xpath("//a[@class='badge-card__section badge-card__section--hoverable']/@href").extract_first()
         company_name = response.xpath("//span[@class='multi-size-header__big']/text()").extract_first()
-        comments = response.xpath("//p[@class='review-content__text']/text()").extract()
-        comments = [comment.strip() for comment in comments]
+        comments = response.xpath("//p[@class='review-content__text']")
+        comments = [comment.xpath('.//text()').extract() for comment in comments]
+        comments = [[c.strip() for c in comment_list] for comment_list in comments]
+        comments = [' '.join(comment_list) for comment_list in comments]
 
         ratings = response.xpath("//div[@class='star-rating star-rating--medium']//img/@alt").extract()
         ratings = [int(re.match('\d+', rating).group(0)) for rating in ratings]
